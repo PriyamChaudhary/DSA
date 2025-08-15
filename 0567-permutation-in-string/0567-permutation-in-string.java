d0425ac8a48d68edd443bbc292402090dcfa72e2
Priyam Chaudhary <chaudharypriyam15@gmail.com>
2025-08-15 22:11:56 +0530

    Time: 6 ms (87.68%), Space: 42.6 MB (89.81%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String p, String s) {
+        boolean ans=false;
+        int[] pmap=new int[26];
+        int[] smap=new int[26];
+        int r=0;
+        int l=0;
+        for(char ch:p.toCharArray()){
+            pmap[ch - 'a']++; //to increase freq of char in p
+        }
+        while(r<s.length()){
+            smap[s.charAt(r) - 'a']++;
+           while(r-l+1>p.length()){
+            smap[s.charAt(l) - 'a']--; //is window size exceeds p length then decrease the freq and shift the window
+            l++;
+        }
+        if(r-l+1==p.length() && Arrays.equals(pmap,smap)){
+            ans=true;
+        }
+        r++;
+        }
+    return ans;
+    }
+}
